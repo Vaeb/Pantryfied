3a66fe79b95c6e3727ce2f3fbbae13928cc3f60c
Vaeb <vaebmail@gmail.com>
2019-02-21 19:25:54 +0000

    Debugging unnecessary

--- backend/src/resolvers/recipe.js
+++ backend/src/resolvers/recipe.js
@@ -10,19 +10,13 @@ export default {
     },
     Mutation: {
         createRecipe: async (parent, args, { models }) => {
-            console.log(1);
             try {
-                console.log(2);
                 const recipe = await models.Recipe.create({
                     ...args,
                 });
-                console.log(3);
-                console.log(recipe);
 
                 return { ok: true, recipe };
             } catch (err) {
-                console.log(4);
-                console.log(err);
                 return { ok: false, errors: [{ path: 'Database recipe insertion', message: String(err) }] };
             }
         },
