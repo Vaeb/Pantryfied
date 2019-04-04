42bcd0986dd543e9fb9323577c3c2eb2f76da706
FisherLoch <lochlainn.fisher@gmail.com>
2019-04-04 20:01:14 +0100

    Comments explaining what needs to be done

--- frontend/mobile_client/src/screens/RecipeScreen.js
+++ frontend/mobile_client/src/screens/RecipeScreen.js
@@ -10,6 +10,16 @@ export default class RecipeScreen extends Component {
     this.navigateInstructionsPressed = this.navigateInstructionsPressed.bind(this);
   }
 
+  /*
+   * The recipe object will be stored in this.context.renderRecipe
+   * Not sure what the object layout is yet but it will probably be in disc by the time you do this
+   * This page will probably just consist of mostly <Text> tags to display all of the info, but possibly an <Image> as well
+   * Will mainly just be styling that needs to be done, theres a button already on the screen which you can design
+   * By passing in the arguments in components/common/Button.js that you can see
+   * The component is quite customisable so shouldnt be hard to understand, the navigation to the instructions screen is already done
+   * So you dont have to worry about that
+   */
+
   navigateInstructionsPressed() {
     this.props.navigation.navigate("InstructionsScreen");
   }
