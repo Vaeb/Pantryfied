4b22a1eaeede4c117f036900ea8e8eb6f6441774
Tyler Jay <tylerjay2704@gmail.com>
2019-04-16 14:06:57 +0100

    Recipe and instruction screen formatting and layout

--- frontend/mobile_client/src/screens/InstructionsScreen.js
+++ frontend/mobile_client/src/screens/InstructionsScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { PantryfiedContext } from '../context/PantryfiedContext';
 
 // eslint-disable-next-line react/prefer-stateless-function
@@ -13,10 +13,38 @@ export default class InstructionsScreen extends Component {
   render() {
     return (
       <View>
-        <Text> Instructions Screen </Text>
+        <Text style={styles.nameText}>{this.context.renderRecipe.name}</Text>
+        <Text style={styles.headerText}>Ingredients</Text>
+        <Text style={styles.instructionText}>{this.context.renderRecipe.ingredients}</Text>
+        <Text style={styles.headerText}>Instructions</Text>
+        <Text style={styles.instructionText}>{this.context.renderRecipe.directions}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+
+      nameText: {
+          fontSize: 30,
+          color: "#FFFFFF",
+          backgroundColor: "#28BAA5",
+          textAlign: "center",
+          height: 50,
+          alignItems: "center",
+        },
+
+      instructionText: {
+         fontSize: 20,
+         color: "black",
+      },
+
+      headerText: {
+        fontSize: 22,
+        color: "black",
+        fontWeight: "bold",
+      }
+
+});
 
 InstructionsScreen.contextType = PantryfiedContext;
--- frontend/mobile_client/src/screens/RecipeScreen.js
+++ frontend/mobile_client/src/screens/RecipeScreen.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { PantryfiedContext } from '../context/PantryfiedContext';
 import { Button } from '../components/common/Button';
+import {Image} from 'react-native' ;
+
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class RecipeScreen extends Component {
@@ -27,16 +29,39 @@ export default class RecipeScreen extends Component {
   navigateInstructionsPressed() {
     this.props.navigation.navigate("InstructionsScreen");
   }
+
+
+
   render() {
     let toRenderRecipe = JSON.stringify(this.context.renderRecipe);
     return (
       <View>
-        <Text> Recipe Screen </Text>
-        <Text>{toRenderRecipe}</Text>
-        <Text>{this.context.renderRecipe.description}</Text>
-        <Button onPress={this.navigateInstructionsPressed} inheritStyle={styles.navButton} inheritTextStyle={styles.navButtonText}>
-          Navigate to Instructions Screen
-        </Button>
+        <Text allowFontScaling adjustsFontSizeToFit numberOfLines={1} style={styles.nameText}>{this.context.renderRecipe.title}</Text>
+        <Image
+            source={{uri: this.context.renderRecipe.imgUrl}}
+            style={{width: 200, height: 200,}}
+        />
+        <Text style={styles.descriptionText}>{this.context.renderRecipe.desc}</Text>
+        <Text style={styles.ratingText}>
+            Rating: {this.context.renderRecipe.rating}/5
+        </Text>
+        <Text></Text>
+        <Text style={styles.infoText}>
+            Calories: {this.context.renderRecipe.calories}
+        </Text>
+        <Text style={styles.infoText}>
+            Protein: {this.context.renderRecipe.protein}g
+        </Text>
+        <Text style={styles.infoText}>
+            Fat: {this.context.renderRecipe.fat}g
+        </Text>
+        <Text style={styles.infoText}>
+            Sodium: {this.context.renderRecipe.sodium}g
+        </Text>
+        <Text></Text>
+        <TouchableOpacity onPress={this.navigateInstructionsPressed} style={styles.navButton} activeOpacity = {.5}>
+          <Text  adjustsFontSizeToFit numberOfLines={1} style={styles.navButtonText}>View Instructions</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -46,14 +71,58 @@ export default class RecipeScreen extends Component {
 const styles = StyleSheet.create({
   navButton: {
     flex: 1,
+    backgroundColor: "#1F7C71",
+    marginBottom: 10,
+    width: 300,
+    alignSelf: 'center',
+    paddingHorizontal: 50,
+
+    paddingTop: 5,
+    paddingBottom:30,
+    marginLeft:30,
+    marginRight:30,
+    backgroundColor:"#1F7C71",
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: "#1F7C71",
   },
+
   navButtonText: {
-    fontSize: 18,
+    fontSize: 15,
+    color: "#FFFFFF",
+    textAlign:"center",
   },
 
   descriptionText: {
     fontSize: 18,
+    fontStyle: "italic",
+    textAlign: "center",
+    textAlignVertical: "center",
+  },
+
+  nameText: {
+      fontSize: 25,
+
+      color: "#FFFFFF",
+      backgroundColor: "#28BAA5",
+      height: 50,
+      alignItems: "center",
+      textAlign: "center",
+      textAlignVertical:"center"
+    },
+
+  ratingText: {
+    fontSize: 22,
+    textAlign: "center",
+    color: "black",
+  },
+
+  infoText: {
+    fontSize: 22,
+    textAlign: "center",
+    color: "black",
   }
+
 });
 
 RecipeScreen.contextType = PantryfiedContext;
