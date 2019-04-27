30d662795ac500cdc8775b7251e7bff9d891fe94
Jahan <jahanulhaque@gmail.com>
2019-04-27 20:46:42 +0100

    account deets

--- frontend/mobile_client/src/screens/AccountDetailsScreen.js
+++ frontend/mobile_client/src/screens/AccountDetailsScreen.js
@@ -1,24 +1,96 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Dialog from 'react-native-dialog';
 import { PantryfiedContext } from '../context/PantryfiedContext';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class AccountDetailsScreen extends Component {
   constructor(props) {
     super(props);
+    this.showDialog = this.showDialog.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
 
     // obtain stored data
     this.state = {
       details: {},
+
+      flatList: [
+        { key: 'First name' },
+        { key: 'Second name' },
+        { key: 'Date of birth' },
+        { key: 'Gender' },
+        { key: 'height' },
+        { key: 'Weight' },
+      ],
+
+      dialogVisibleArray: [false, false, false, false, false, false],
+      index: 0,
     };
   }
 
+  showDialog(i) {
+    const newArray = [this.dialogVisibleArray];
+    newArray[i] = true;
+    this.setState({ dialogVisibleArray: newArray });
+  }
+
+  handleCancel(i) {
+    const newArray = [this.dialogVisibleArray];
+    newArray[i] = false;
+    this.setState({ dialogVisibleArray: newArray });
+  }
+
+  handleUpdate(i) {
+    // TODO
+    console.log(i);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  alertBox(item) {
+    for (let i = 0; i < this.state.flatList.length; i++) {
+      if (item.key == this.state.flatList[i].key) {
+        this.showDialog(i);
+      }
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  renderButton(item) {
+    return (
+      <View>
+        <TouchableOpacity onPress={() => this.alertBox(item)}>
+          <Text style={styles.text}>{item.key}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   render() {
-    return <Text>Account details</Text>;
+    let title = 'Enter ';
+    for (let i = 0; i < this.state.dialogVisibleArray.length; i++) {
+      if (this.state.dialogVisibleArray[i] == true) {
+        title += this.state.flatList[i].key;
+        this.state.index = i;
+      }
+    }
+    return (
+      <View>
+        <Dialog.Container visible={this.state.dialogVisibleArray.includes(true)}>
+          <Dialog.Title>{title}</Dialog.Title>
+          <Dialog.Description>Do you want to delete this account? You cannot undo this action.</Dialog.Description>
+          <Dialog.Input />
+          <Dialog.Button label="Cancel" onPress={this.handleCancel} />
+          <Dialog.Button label="Update" onPress={this.handleUpdate} />
+        </Dialog.Container>
+        <Text style={styles.headerBar}> Personal details </Text>
+        <FlatList data={this.state.flatList} renderItem={({ item }) => this.renderButton(item)} />
+      </View>
+    );
   }
 }
 
-AccountDetailsScreen.contextType = PantryfiedContext;
+PersonalDetailsScreen.contextType = PantryfiedContext;
 
 const styles = StyleSheet.create({
   MainContainer: {
